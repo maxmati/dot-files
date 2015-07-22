839ac2307e4133a1b50a00f9da9d25740f04a8f8
Mateusz  "Maxmati" Nowotyński <admin@maxmati.pl>
2015-07-22 23:17:22 +0200

    Git aware fish prompt.

--- src/config/fish/config.fish
+++ src/config/fish/config.fish
@@ -7,3 +7,33 @@ set PATH $PATH $NPM_PACKAGES/bin
 #set MANPATH "$NPM_PACKAGES/share/man:$(manpath)"
 set NODE_PATH "$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
 
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_untrackedfiles '☡'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
+
+
+function fish_prompt
+  set last_status $status
+
+  set_color $fish_color_cwd
+  printf '%s' (prompt_pwd)
+  set_color normal
+
+  printf '%s ' (__fish_git_prompt)
+
+  set_color normal
+end
+
