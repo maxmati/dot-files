91f8411d72904009d7f6ecef2d3cd170c80c26de
Mateusz Nowotyński <admin@maxmati.pl>
2017-03-10 18:49:53 +0100

    Modify path

--- src/config/fish/config.fish
+++ src/config/fish/config.fish
@@ -12,7 +12,7 @@ alias grep="grep --color=auto"
 set GREP_OPTIONS
 
 set NPM_PACKAGES "$HOME/.npm-packages"
-set PATH $PATH $NPM_PACKAGES/bin
+set PATH $PATH $NPM_PACKAGES/bin "/usr/local/bin"
 #set MANPATH "$NPM_PACKAGES/share/man:$(manpath)"
 set NODE_PATH "$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
 
