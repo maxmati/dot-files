3f5978f1afe7fc4577a9ee1a2aadcabca6d80932
Mateusz "maxmati" Nowotyński <maxmati4@gmail.com>
2017-04-06 00:14:32 +0200

    Xmonad tray

--- src/xmonad/xmonad.hs
+++ src/xmonad/xmonad.hs
@@ -1,6 +1,7 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.SetWMName
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
@@ -8,11 +9,13 @@ import System.IO
 main = do
     xmproc <- spawnPipe "/usr/bin/xmobar /home/maxmati/.xmobarrc"
     spawn "nm-applet"
+    spawn "stalonetray"
     xmonad $ defaultConfig
         { manageHook = manageDocks <+> manageHook defaultConfig
         , layoutHook = avoidStruts  $  layoutHook defaultConfig
 	, modMask = mod4Mask
 	, terminal = "terminator"
+	, startupHook = setWMName "LG3D"
 	, logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "green" "" . shorten 50
