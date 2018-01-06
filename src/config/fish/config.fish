6920a123e2057b7f71cf3eba51ada55f4411c8cd
Mateusz Nowotyński <maxmati@memleak.pl>
2018-01-06 15:41:18 +0100

    Reconfigure leopardus

--- src/config/fish/config.fish
+++ src/config/fish/config.fish
@@ -7,7 +7,7 @@ if status --is-interactive
 	keychain --eval --quiet -Q id_ed25519 -Q id_rsa | source
 end
 
-set PATH $PATH ~/bin ~/.gem/ruby/*/bin/ ~/.local/bin
+set PATH $PATH ~/bin ~/.gem/ruby/2.4.0/bin/ ~/.local/bin
 alias grep="grep --color=auto"
 set GREP_OPTIONS
 
@@ -17,7 +17,7 @@ set PATH $PATH $NPM_PACKAGES/bin "/usr/local/bin"
 set NODE_PATH "$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
 
 # Fish git prompt
-set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showdirtystate 'no'
 set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showuntrackedfiles 'yes'
 set __fish_git_prompt_showupstream 'yes'
@@ -26,7 +26,7 @@ set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
 
 # Status Chars
-set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_dirtystate ''
 set __fish_git_prompt_char_stagedstate '→'
 set __fish_git_prompt_char_untrackedfiles '☡'
 set __fish_git_prompt_char_stashstate '↩'
